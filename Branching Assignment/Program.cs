e4d9b3e1a2dd8cec8dd7664dcb148ab45ce6ad99
stephyannjose <stephyannjose@gmail.com>
2023-12-12 22:07:59 -0400

    Add project files.

---
+++ Branching Assignment/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Welcome message
+        Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+        // Prompt for package weight
+        Console.WriteLine("Please enter the package weight:");
+        int weight = Convert.ToInt32(Console.ReadLine());
+
+        // Check if the weight is too heavy
+        if (weight > 50)
+        {
+            Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            return; // Exit the program
+        }
+
+        // Prompt for package dimensions
+        Console.WriteLine("Please enter the package width:");
+        int width = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Please enter the package height:");
+        int height = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Please enter the package length:");
+        int length = Convert.ToInt32(Console.ReadLine());
+
+        // Check if the dimensions are too big
+        int dimensionsTotal = width + height + length;
+        if (dimensionsTotal > 50)
+        {
+            Console.WriteLine("Package too big to be shipped via Package Express.");
+            return; // Exit the program
+        }
+
+        // Calculate the quote
+        decimal quote = (width * height * length * weight) / 100.0m;
+
+        // Display the quote to the user
+        Console.WriteLine($"Your estimated total for shipping this package is: ${quote:F2}");
+        Console.WriteLine("Thank you!");
+    }
+}
